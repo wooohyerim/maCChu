1b378b32da0d079b20b8be25ba943ce0859734d4
woohyerim <hyerim3414@gmail.com>
2022-08-22 17:08:06 +0900

    Add: nav 파일 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <NavBox>
+      <Logo>
+        ma<span>CC</span>hu
+      </Logo>
+      <ul>
+        <li>리스트</li>
+        <li>마이페이지</li>
+        <li>카카오 로그인</li>
+      </ul>
+    </NavBox>
+  );
 };
 
 export default Nav;
+
+const NavBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  background-color: #e4971c;
+`;
+
+const Logo = styled.h1`
+  font-size: 20px;
+  color: #fff;
+`;
