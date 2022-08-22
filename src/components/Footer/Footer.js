3a3a5c45f19e857be555946039c17f4b276a7421
woohyerim <hyerim3414@gmail.com>
2022-08-22 20:49:49 +0900

    Add: footer 레이아웃

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterBox>
+      <Logo>
+        ma<span>CC</span>hu
+      </Logo>
+      <CopyBox>
+        <div>Copyright &copy; 2022 maCChu. All Rights Reserved.</div>
+        <div>
+          본 사이트는 미니 프로젝트 목적으로 제작되어 상업적 목적이 아님을
+          밝힙니다.
+        </div>
+      </CopyBox>
+    </FooterBox>
+  );
 };
 
 export default Footer;
+
+const FooterBox = styled.div`
+  width: 100%;
+  height: 200px;
+  padding: 10px;
+  background-color: #ddd;
+  font-size: 14px;
+  line-height: 2;
+`;
+
+const Logo = styled.h1`
+  @font-face {
+    font-family: 'SBAggroB';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  color: #fff;
+  font-family: 'SBAggroB';
+  font-size: 30px;
+`;
+
+const CopyBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 30px;
+  color: #ccc;
+`;
