a7fa77168b1ec303d2937fce7a29effbafd582dd
woohyerim <hyerim3414@gmail.com>
2022-08-22 17:29:01 +0900

    Add: font

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,9 +4,23 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
+@font-face {
+    font-family: 'paybooc-Medium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/paybooc-Medium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 
 *{
   box-sizing: border-box;
+}
+
+body{
+  font-family:  'paybooc-Medium';
+}
+
+ul,li{
+  text-decoration: none;
 }
 `;
 
