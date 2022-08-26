24d209d534af1787c37d125d686f20aee877fdad
woohyerim <hyerim3414@gmail.com>
2022-08-26 16:37:30 +0900

    Modify: 레이아웃 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 const Footer = () => {
   return (
     <FooterBox>
-      <h1>
-        <img src="/images/macchu_logo.png" alt="" />
-      </h1>
+      {/* <h1>
+        <Logo src="/images/macchu_logo.png" alt="" />
+      </h1> */}
       <CopyBox>
         <div>Copyright &copy; 2022 maCChu. All Rights Reserved.</div>
         <div>
@@ -23,30 +23,19 @@ export default Footer;
 const FooterBox = styled.div`
   width: 100%;
   height: 200px;
-  padding: 10px;
-  border-top: 1px solid #e6e6e6;
-  /* background-color: #e4971c; */
+  background-color: #eee;
   font-size: 14px;
   line-height: 2;
 `;
 
-// const Logo = styled.h1`
-//   @font-face {
-//     font-family: 'SBAggroB';
-//     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff')
-//       format('woff');
-//     font-weight: normal;
-//     font-style: normal;
-//   }
-//   color: #fff;
-//   font-family: 'SBAggroB';
-//   font-size: 30px;
-// `;
+const Logo = styled.img`
+  width: 200px;
+`;
 
 const CopyBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 50px;
-  color: #333;
+  padding-top: 60px;
+  color: #ccc;
 `;
